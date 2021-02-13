6e6e804fedccc779fba596d8a1841dc4f33f47bc
BeerOnTheBeach <gerproe@googlemail.com>
2021-02-13 12:11:52 +0100

    Fix logo path

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Jojos Numbers</title>
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="src/css/bootstrap.min.css">
     <link rel="stylesheet" href="src/css/main.css">
@@ -14,7 +14,7 @@
     <script src="src/js/jquery-3.5.1.min.js"></script>
     <script src="src/js/bootstrap.min.js"></script>
 
-    <link rel="icon" type="image/png" href="/jojosnumbers/public/src/images/favicon.jpg"/>
+    <link rel="icon" type="image/png" href="/public/src/images/favicon.jpg"/>
 </head>
 <body>
 <?php include 'src/snippets/header.php';
