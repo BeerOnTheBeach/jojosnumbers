73432189367e16e18e8fb0ea8f6515a2038cb0ad
root <root@h2908085.stratoserver.net>
2021-02-13 12:24:25 +0100

    add markdown-file

---
+++ markdown.sh
@@ -0,0 +1,3 @@
+Rscript -e "rmarkdown::render('/var/www/jojosnumbers/public/src/statistics/durak/durak.Rmd')"
+Rscript -e "rmarkdown::render('/var/www/jojosnumbers/public/src/statistics/ultimate/ultimate.Rmd')"
+
