eb175b91e2f4faa60c93edc2c10004cb930f954b
BeerOnTheBeach <gerproe@googlemail.com>
2021-02-15 13:18:33 +0100

    [TASK] Add relative paths for links and logo-src

--- public/src/snippets/header.php
+++ public/src/snippets/header.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-inverse">
-    <a class="navbar-brand" href="index.php">
-        <img class="logo" alt="Logo" src="src/images/logo.gif">
+    <a class="navbar-brand" href="/">
+        <img class="logo" alt="Logo" src="/src/images/logo.gif">
     </a>
     <div class="container">
         <div class="navbar-header">
@@ -11,9 +11,9 @@
                 <span class="icon-bar"></span>
             </button>
             <ul class="nav navbar-nav">
-                <li><a href="src/statistics/durak/durak.html">Durak <span class="sr-only">(current)</span></a></li>
-                <li><a href="src/statistics/ultimate/ultimate.html">Smäsh <span class="sr-only">(current)</span></a></li>
-                <li><a href="src/durakApp/index.php">Durak App <span class="sr-only">(current)</span></a></li>
+                <li><a href="/src/statistics/durak/durak.html">Durak <span class="sr-only">(current)</span></a></li>
+                <li><a href="/src/statistics/ultimate/ultimate.html">Smäsh <span class="sr-only">(current)</span></a></li>
+                <li><a href="/src/durakApp/index.php">Durak App <span class="sr-only">(current)</span></a></li>
             </ul>
         </div>
     </div>
