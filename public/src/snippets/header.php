545702800bad8164108c8c38893e24872a360903
BeerOnTheBeach <gerproe@googlemail.com>
2021-02-18 21:00:42 +0100

    Add randomizer to navbar

--- public/src/snippets/header.php
+++ public/src/snippets/header.php
@@ -1,17 +1,47 @@
 <link rel="stylesheet" href="../css/durak.css">
 
-<nav class="navbar navbar-inverse">
-    <a class="navbar-brand" href="/">
-        <img class="logo" alt="Logo" src="/src/images/logo.gif">
-    </a>
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation"">
     <div class="container">
+        <a class="navbar-brand" href="/">
+            <img class="logo" alt="Logo" src="/src/images/logo.gif">
+        </a>
         <div class="navbar-header">
             <ul class="nav navbar-nav">
-                <li><a href="/src/statistics/durak/durak.php">Durak<span class="sr-only">(current)</span></a></li>
-                <li><a href="/src/statistics/ultimate/ultimate.html">Smäsh<span class="sr-only">(current)</span></a></li>
-                <li><a href="/src/durakApp/index.php">Durak App<span class="sr-only">(current)</span></a></li>
-                <li><a href="/src/pokemon-randomizer/public/index.php">Pokemon Randomizer<span class="sr-only">(current)</span></a></li>
+                <li><a href="/src/statistics/durak/durak.php">Durak</a></li>
+                <li><a href="/src/statistics/ultimate/ultimate.html">Smäsh</a></li>
+                <li><a href="/src/durakApp/index.php">Durak App</a></li>
+
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="/src/pokemon-randomizer/public/index.php">Pokemon Randomizer</a></li>
             </ul>
         </div>
     </div>
 </nav>
+
+<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="/">
+                <img class="logo" alt="Logo" src="/src/images/logo.gif">
+            </a>
+        </div>
+        <div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li><a href="/src/statistics/durak/durak.php">Durak</a></li>
+                <li><a href="/src/statistics/ultimate/ultimate.html">Smäsh</a></li>
+                <li><a href="/src/durakApp/index.php">Durak App</a></li>
+
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="/src/pokemon-randomizer/public/index.php">Pokemon Randomizer</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
