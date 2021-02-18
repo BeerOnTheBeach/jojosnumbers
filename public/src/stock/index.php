7a8f2fd4f3db90df81b55ec68937ae3394e20811
BeerOnTheBeach <gerproe@googlemail.com>
2021-02-18 19:12:01 +0100

    [TASK] auto-stock im arsch

---
+++ public/src/stock/index.php
@@ -0,0 +1,27 @@
+<?php
+require_once '../../../vendor/autoload.php';
+
+// Option
+$option = new AlphaVantage\Options();
+$option->setApiKey('DFO0621J5JWADO97');
+
+// Client
+$client = new AlphaVantage\Client($option);
+$dailyXaiomi =$client->timeseries()->dailyAdjusted("XIACF")["Time Series (Daily)"];
+
+$symbole = ["XIACF", "AAPL", "SNE"];
+
+foreach ($symbole as $symbol) {
+    $result = $client->timeseries()->dailyAdjusted("$symbol")["Time Series (Daily)"];
+    $exchangeRate = $client->foreignExchange()->daily("USD", "EUR");
+
+    foreach($result as $date => $item) {
+        $closed = $item['4. close'];
+        var_dump($exchangeRate[$date]); die();
+        $closed2 = $closed*$exchangeRate[$date][['4. close']];
+        echo "$date: $closed $<br>";
+        echo "$date: $closed2 €";
+    }
+    echo "Neues symbol <br>";
+}
+
